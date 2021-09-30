6c9685a4aa453cc518d9f26d87dc7c719cf70688
Mahmoud217TR <faafet.mahmoud@gmail.com>
2021-09-30 17:23:43 +0300

    divided into 4 files

--- main.py
+++ main.py
@@ -1,32 +1,6 @@
-import re
-
-# to get the original image if there is another secret message in it
-def getImage(url):
-    try:
-        h = open(url, 'rb')
-        data = h.read().hex()
-        r = re.search('ffd9', data)
-        end = r.span()[1]
-        return (bytes.fromhex(data[:end]))
-    except:
-        print("Couldn't read File: File not Found or Image Corrupted!")
-        exit(-1)
-
-# appending data after image data
-def addOnImage(data,string):
-    data += string.encode('utf-8')
-    return data
-
-# writing a new image
-def writeNewImage(url,data):
-    try:
-        if url[-4:] != '.jpg':
-            url+='.jpg'
-        new = open(url, 'wb')
-        new.write(data)
-    except:
-        print('Failed to write a new Image!')
-
+from read import getImage
+from add import  addOnImage
+from write import writeNewImage
 
 if __name__ == '__main__':
     ogURL = input('Enter the jpg photo name: ')
